8fa6a88cf43c5b1cb68c3aa15802049cbed6650e
TheRK420 <riksengupta458@gmail.com>
2024-03-11 16:59:04 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const LoremGenerator = () => {
+  const [numParagraphs, setNumParagraphs] = useState(1);
+  const [generatedText, setGeneratedText] = useState('');
+
+  // Function to generate Lorem Ipsum text
+  const generateLoremText = () => {
+    fetch(`https://baconipsum.com/api/?type=all-meat&paras=${numParagraphs}&start-with-lorem=1`)
+      .then(response => response.json())
+      .then(data => {
+        const paragraphs = data.map((paragraph, index) => <p key={index}>{paragraph}</p>);
+        setGeneratedText(paragraphs);
+      })
+      .catch(error => {
+        console.error('Error fetching Lorem Ipsum text:', error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main'>
+      <h1>Lorem Generator</h1>
+      <label className='texthead' htmlFor="numParagraphs">Number of Paragraphs:</label>
+      <input className='inputbox'
+        type="number"
+        id="numParagraphs"
+        value={numParagraphs}
+        onChange={e => setNumParagraphs(parseInt(e.target.value))}
+      />
+      <button onClick={generateLoremText}>Generate</button>
+      <div>
+        <h2>Generated Text:</h2>
+        {generatedText}
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default LoremGenerator;
